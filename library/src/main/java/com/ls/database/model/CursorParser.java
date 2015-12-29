877ecf6b576b41e9faa415806a1ce6fea342b83e
Stanislav Bodnar <stanislav.bodnar@lemberg.co.uk>
2015-12-29 13:59:42 +0200

    Updated cursor parser.

--- library/src/main/java/com/ls/database/model/CursorParser.java
+++ library/src/main/java/com/ls/database/model/CursorParser.java
@@ -37,39 +37,119 @@ public class CursorParser {
     }
 
     public String readString(String columnName) {
-        return mCursor.getString(mCursor.getColumnIndex(columnName));
+        return readString(mCursor.getColumnIndex(columnName));
     }
 
     public Byte readByte(String columnName) {
-        return (byte) mCursor.getInt(mCursor.getColumnIndex(columnName));
+        return readByte(mCursor.getColumnIndex(columnName));
     }
 
     public Short readShort(String columnName) {
-        return mCursor.getShort(mCursor.getColumnIndex(columnName));
+        return readShort(mCursor.getColumnIndex(columnName));
     }
 
     public Integer readInteger(String columnName) {
-        return mCursor.getInt(mCursor.getColumnIndex(columnName));
+        return readInteger(mCursor.getColumnIndex(columnName));
     }
 
     public Long readLong(String columnName) {
-        return mCursor.getLong(mCursor.getColumnIndex(columnName));
+        return readLong(mCursor.getColumnIndex(columnName));
     }
 
     public Float readFloat(String columnName) {
-        return mCursor.getFloat(mCursor.getColumnIndex(columnName));
+        return readFloat(mCursor.getColumnIndex(columnName));
     }
 
     public Double readDouble(String columnName) {
-        return mCursor.getDouble(mCursor.getColumnIndex(columnName));
+        return readDouble(mCursor.getColumnIndex(columnName));
     }
 
     public Boolean readBoolean(String columnName) {
-        return mCursor.getInt(mCursor.getColumnIndex(columnName)) > 0;
+        return readBoolean(mCursor.getColumnIndex(columnName));
     }
 
     public byte[] readBlob(String columnName) {
-        return mCursor.getBlob(mCursor.getColumnIndex(columnName));
+        return readBlob(mCursor.getColumnIndex(columnName));
+    }
+
+    public String readString(int columnIndex) {
+        return mCursor.getString(columnIndex);
+    }
+
+    public Byte readByte(int columnIndex) {
+        return (byte) mCursor.getInt(columnIndex);
+    }
+
+    public Short readShort(int columnIndex) {
+        return mCursor.getShort(columnIndex);
+    }
+
+    public Integer readInteger(int columnIndex) {
+        return mCursor.getInt(columnIndex);
+    }
+
+    public Long readLong(int columnIndex) {
+        return mCursor.getLong(columnIndex);
+    }
+
+    public Float readFloat(int columnIndex) {
+        return mCursor.getFloat(columnIndex);
+    }
+
+    public Double readDouble(int columnIndex) {
+        return mCursor.getDouble(columnIndex);
+    }
+
+    public Boolean readBoolean(int columnIndex) {
+        return mCursor.getInt(columnIndex) > 0;
+    }
+
+    public byte[] readBlob(int columnIndex) {
+        return mCursor.getBlob(columnIndex);
+    }
+
+    public int getColumnIndex(String columnName) {
+        return mCursor.getColumnIndex(columnName);
+    }
+
+    public int getCount() {
+        return mCursor.getCount();
+    }
+
+    public int getPosition() {
+        return mCursor.getPosition();
+    }
+
+    public boolean isFirst() {
+        return mCursor.isFirst();
+    }
+
+    public boolean isLast() {
+        return mCursor.isLast();
+    }
+
+    public boolean isBeforeFirst() {
+        return mCursor.isBeforeFirst();
+    }
+
+    public boolean isAfterLast() {
+        return mCursor.isAfterLast();
+    }
+
+    public String[] getColumnNames() {
+        return mCursor.getColumnNames();
+    }
+
+    public int getColumnCount() {
+        return mCursor.getColumnCount();
+    }
+
+    public boolean isNull(int columnIndex) {
+        return mCursor.isNull(columnIndex);
+    }
+
+    public boolean isClosed() {
+        return mCursor.isClosed();
     }
 
     /**
